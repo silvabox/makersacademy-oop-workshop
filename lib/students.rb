4fa5015f5d4a5b00adada84dcdd990194787546f
Ben Forrest <ben@silvabox.com>
2016-09-29 15:52:36 +0100

    use default to_s of student in output

--- lib/students.rb
+++ lib/students.rb
@@ -6,6 +6,4 @@ students = YAML.load_file('students.yml')
 students.map! { |hash| Student.new(hash) }
 
 
-students.sort.each do |student|
-  puts "#{student.full_name}, rating: #{student.rating}"
-end
+students.sort.each { |student| puts student }
