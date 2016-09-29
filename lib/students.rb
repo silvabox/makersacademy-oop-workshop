471319d39134337051748baba13d957de3c7358b
Ben Forrest <ben@silvabox.com>
2016-09-29 14:36:03 +0100

    implements student comparison in students

--- lib/students.rb
+++ lib/students.rb
@@ -6,10 +6,6 @@ students = YAML.load_file('students.yml')
 students.map! { |hash| Student.new(hash) }
 
 
-students.sort! do |a, b|
-  a.rating <=> b.rating
-end
-
-students.each do |student|
+students.sort.each do |student|
   puts "#{student.full_name}, rating: #{student.rating}"
 end
