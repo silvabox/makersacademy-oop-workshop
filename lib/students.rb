6d72ed9189cac5082648732598efcabe72801a0b
Ben Forrest <ben@silvabox.com>
2016-09-29 14:15:06 +0100

    implements student class in students

--- lib/students.rb
+++ lib/students.rb
@@ -15,21 +15,18 @@ def rating_for_student(full_name, year_of_birth, height)
 end
 
 students.sort! do |a, b|
-  full_name_a = full_name(a['first_name'], a['last_name'])
-  full_name_b = full_name(b['first_name'], b['last_name'])
-  year_of_birth_a = Date.parse(a['date_of_birth']).year
-  year_of_birth_b = Date.parse(b['date_of_birth']).year
+  full_name_a = full_name(a.first_name, a.last_name)
+  full_name_b = full_name(b.first_name, b.last_name)
 
-  rating_a = rating_for_student(full_name_a, year_of_birth_a, a['height'])
-  rating_b = rating_for_student(full_name_b, year_of_birth_b, b['height'])
+  rating_a = rating_for_student(full_name_a, a.date_of_birth.year, a.height)
+  rating_b = rating_for_student(full_name_b, b.date_of_birth.year, b.height)
 
   rating_a <=> rating_b
 end
 
-students.each do |hash|
-  full_name = full_name(hash['first_name'], hash['last_name'])
-  year_of_birth = Date.parse(hash['date_of_birth']).year
-  rating = rating_for_student(full_name, year_of_birth, hash['height'])
+students.each do |student|
+  full_name = full_name(student.first_name, student.last_name)
+  rating = rating_for_student(full_name, student.date_of_birth.year, student.height)
 
   puts "#{full_name}, rating: #{rating}"
 end
